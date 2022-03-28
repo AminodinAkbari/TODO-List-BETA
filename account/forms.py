3c42f1d81ca297e984af3db04f178e2a14ee3176
Amin Akbari <13amin80badboy@gmail.com>
2022-03-28 17:52:19 +0430

    Amin

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
 
 class Registerforms(forms.Form):
 	username = forms.CharField(widget = forms.TextInput(attrs={'placeholder':'username' , 'class':'custom_input'}))
--- account/views.py
+++ account/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render , redirect
 from .forms import *
 from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def login_view(request):
+
 	if request.user.is_authenticated:
 		return redirect('/')
 	form = Loginforms(request.POST or None)
-	register_form = Registerforms(request.POST or None)
 	if form.is_valid():
 		username = form.cleaned_data.get('username')
 		password = form.cleaned_data.get('password')
@@ -16,6 +17,22 @@ def login_view(request):
 		if user is not None:
 			login(request , user)
 			return redirect('/')
+		else:
+			messages.error(request,'username or password not correct')
+			return redirect('login_view')
+
+	context = {
+	'form':form,
+	}
+	return render(request , 'Login-Template/index-login.html' , context)
+
+
+def register_view(request):
+	
+	if request.user.is_authenticated:
+		return redirect('/')
+
+	register_form = Registerforms(request.POST or None)
 	if register_form.is_valid():
 		print('register_is valid')
 		username = register_form.cleaned_data.get('username')
@@ -27,7 +44,6 @@ def login_view(request):
 			return redirect('/')
 
 	context = {
-	'form':form,
 	'register_form':register_form
 	}
-	return render(request , 'dist/index.html' , context)
+	return render(request , 'Login-Template/index-register.html' , context)
